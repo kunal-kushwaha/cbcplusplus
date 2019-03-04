172c342a11e9cc71309a5b844436430a557f9d93
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-03-04 22:46:54 +0530

    initial commit

---
+++ Basics/cmake-build-debug/CMakeFiles/Basics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Basics.dir/main.cpp.o"
+  "Basics.pdb"
+  "Basics"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
